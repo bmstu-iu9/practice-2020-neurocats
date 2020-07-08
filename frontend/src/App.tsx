06574721b5d370aac5e6864e25ef2d6eccda2814
Jul Hrobak <yukhrobak@gmail.com>
2020-07-09 01:23:04 +0300

    Initialize monorepo project with frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NeuroCats</h1>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
